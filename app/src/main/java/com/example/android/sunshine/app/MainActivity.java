89490c2cb4e8f36255e4b6ef0c74622c6df94ef7
stefan <stefan_verhoeff@yahoo.com>
2017-08-23 22:29:21 +0200

    Formatting

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity {
 
     static String LOG_TAG = MainActivity.class.getCanonicalName();
