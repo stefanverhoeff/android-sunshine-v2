d95b8480b20df649d217305c35666dd092e3f698
stefan <stefan_verhoeff@yahoo.com>
2017-10-01 12:43:05 +0200

    Formatting

--- app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
+++ app/src/main/java/com/example/android/sunshine/app/WeatherDataParser.java
@@ -16,10 +16,11 @@ class WeatherDataParser {
         this.weatherSettings = weatherSettings;
     }
 
-    /* The date/time conversion code is going to be moved outside the asynctask later,
-         * so for convenience we're breaking it out into its own method now.
-         */
-    private String getReadableDateString(long time){
+    /*
+     * The date/time conversion code is going to be moved outside the asynctask later,
+     * so for convenience we're breaking it out into its own method now.
+     */
+    private String getReadableDateString(long time) {
         // Because the API returns a unix timestamp (measured in seconds),
         // it must be converted to milliseconds in order to be converted to valid date.
         SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
@@ -50,7 +51,7 @@ class WeatherDataParser {
     /**
      * Take the String representing the complete forecast in JSON Format and
      * pull out the data we need to construct the Strings needed for the wireframes.
-     *
+     * <p>
      * Fortunately parsing is easy:  constructor takes the JSON string and converts it
      * into an Object hierarchy for us.
      */
